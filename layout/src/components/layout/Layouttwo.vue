83b194006ae6dd5d0e2bf681fd1421d70f4a835c
kt79530 <kiteaman82@gmail.com>
2025-05-30 16:12:16 +0900

    Add files via upload

---
+++ layout/src/components/layout/Layouttwo.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-layout class="rounded rounded-md border">
+        <v-navigation-drawer>
+            <v-list nav>
+                <v-list-item title="Navigation drawer" link>
+
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-app-bar title="Application bar"></v-app-bar>
+        <v-main class="d-flex align-cenet justify-center" height="300">
+            <v-container>
+                <v-sheet
+             border="dasded md" 
+             color="suface-light" 
+             height="200" 
+             rounded="1g"
+                >
+             </v-sheet>
+            </v-container>
+        </v-main>
+    
+    </v-layout>
+</template>
---
+++ layout/src/components/nav/Nav4.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-card 
+    class="mx-auto"
+    max-width="430">
+    <v-layout>
+        <v-app-bar color="info" density="prominent">
+        <template v-slot:prepend>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+        <v-app-bar-title>My Recent Trips</v-app-bar-title>
+<template v-slot:append>
+    <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+</template>            
+        </v-app-bar>
+        <v-main>
+            <v-container fluid>
+                <v-card
+                class="mb-2"
+                density="compact"
+                prepend-avatar="https://randomuser.me/api/portraits/women/10.jpg"
+                subtitle="Olivia"
+                variant="text"
+                border>
+            <v-img height="128" src="http://picsum.photos/512/128?image=660" cover></v-img>
+            <v-card-text>Happy New Year!!</v-card-text>
+            <template v-slot:actions>
+                <v-btn color="primary" variant="flat">View more</v-btn>
+                <v-btn color="primary" variant="text">See an Map</v-btn>
+                <v-btn color="primary" variant="tonal">back</v-btn>
+                <v-btn color="primary" variant="outlined">next</v-btn>
+            </template>
+            </v-card>
+
+            </v-container>
+        </v-main>
+    </v-layout>
+    </v-card>
+</template>
